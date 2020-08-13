1c0b807b9e2b596bbb895dc26888f66bb5cbf0a3
serkankorkac <korkacserkan@gmail.com>
2020-08-13 16:31:30 +0300

    content slide eklendşi css ler yazıldı

---
+++ components/ContentSlide.vue
@@ -0,0 +1,30 @@
+<template>
+  <carousel :perPage="1">
+    <slide v-for="item in 10" :key="item">
+      <content-card
+        title="Bölgedeki en iyi yedek parçaları keşfedin"
+        buttontext="Hemen arayın"
+        img="1.png"
+        >Aradığınız bütün orijinal yedek parçaları en uygun fiyatlarla bölgenin
+        en iyi yedek parça marketi Deniz Oto ve Yedek Parça da haftanın her günü
+        bulabilirsiniz.</content-card
+      >
+    </slide>
+  </carousel>
+</template>
+<style lang="less">
+.VueCarousel-dot {
+  width: 20px !important;
+  height: 8px !important;
+  color: @gray !important;
+  background-color: @gray !important;
+  border-radius: 1.5em !important;
+}
+&[aria-selected="true"] {
+  width: 40px !important;
+  height: 8px !important;
+  color: @drop !important;
+  background-color: @drop !important;
+  border-radius: 1.5em !important;
+}
+</style>
--- nuxt.config.js
+++ nuxt.config.js
@@ -30,6 +30,7 @@ export default {
   ** Plugins to load before mounting the App
   */
   plugins: [
+    {src:'@/plugins/carousel.js',ssr:false}
   ],
   /*
   ** Nuxt.js dev-modules
