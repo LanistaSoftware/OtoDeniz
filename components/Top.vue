054ebab89964f0533da5530daed2050bed9bcf52
serkankorkac <korkacserkan@gmail.com>
2020-08-14 15:57:55 +0300

    top component eklendi

---
+++ components/Top.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="top-container">
+    <div class="top-icon text-xdark">
+          <slot name="image"></slot>
+    </div>
+    <div class="top-text">
+      <p class="f_regular text-xdark">
+       <slot name="text"></slot>
+      </p>
+    </div>
+  </div>
+</template>
+<style lang="less" scoped>
+.top-container {
+  display: flex;
+  align-items: center;
+}
+.top-text{
+    display: flex;
+    align-items: flex-end;
+}
+.top-icon {
+  margin-right: 3px;
+  display: flex;
+  align-items: center;
+}
+</style>
