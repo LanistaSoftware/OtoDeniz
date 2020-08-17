86ad7090d7727f4520d562f81552ae02772fec90
serkankorkac <korkacserkan@gmail.com>
2020-08-17 10:38:50 +0300

    header menu active class ayarları yapıldı

--- components/HeaderMenu.vue
+++ components/HeaderMenu.vue
@@ -1,36 +1,53 @@
 <template>
   <div>
     <ul>
-      <li>
-        <nuxt-link tag="a" to="#ansayfa" exact-active-class="">Ana sayfa</nuxt-link>
-        <span />
-      </li>
-      <li>
-        <nuxt-link tag="a" to="#ansayfa" exact-active-class="">Ana sayfa</nuxt-link><span />
-      </li>
-      <li>
-        <nuxt-link tag="a" to="#ansayfa" exact-active-class="">Ana sayfa</nuxt-link><span />
-      </li>
-      <li>
-        <nuxt-link tag="a" to="#ansayfa" exact-active-class="">Ana sayfa</nuxt-link><span />
-      </li>
-      <li>
-        <nuxt-link tag="a" to="#ansayfa" exact-active-class="">Ana sayfa</nuxt-link><span />
+      <li class="f_normal_bold" v-for="(item, i) in menu" :key="i">
+        <nuxt-link :to="'#' + item.link" tag="a" exact
+          >{{ item.text }} <span />
+        </nuxt-link>
       </li>
     </ul>
   </div>
 </template>
+<script>
+export default {
+  data() {
+    return {
+      menu: [
+        {
+          text: "Ana sayfa",
+          link: "anasayfa"
+        },
+        {
+          text: "Hakkımızda",
+          link: "hakkımızda"
+        },
+        {
+          text: "Ürünler",
+          link: "ürünler"
+        },
+        {
+          text: "İstek Listesi",
+          link: "isteklistesi"
+        },
+        {
+          text: "İletişim",
+          link: "iletişim"
+        }
+      ]
+    };
+  }
+};
+</script>
 <style lang="less" scoped>
 li {
   color: @htext;
   float: right;
-  display: flex;
+  display: grid;
+  font-size: 1rem;
   align-items: center;
   justify-content: center;
   padding: 0.5rem;
-}
-span {
-  display: none;
 }
 ul {
   display: flex;
